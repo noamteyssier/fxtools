60d2c08243421966adc158a75fadf2a812e9e8f1
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-05-10 11:11:26 -0700

    19 add in dynamic trimming (#20)
    * bump version
    
    * added trim command as submodule
    
    * added function to trim dynamically within sequence
    
    * ran cargo formatting

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,8 +1,9 @@
-pub mod unique;
-pub mod sgrna_table;
-pub mod upper;
-pub mod reverse;
 pub mod extract;
 pub mod io;
+pub mod reverse;
+pub mod sgrna_table;
+pub mod trim;
+pub mod unique;
+pub mod upper;
 
 pub use io::{match_output_stream, write_output};
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
-use clap::{Parser, Subcommand};
 use anyhow::Result;
+use clap::{Parser, Subcommand};
 
 mod commands;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-
     #[clap(subcommand)]
-    command: Commands
-
+    command: Commands,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-
     /// Filters the Fastx file for Unique Sequences
     Unique {
-
         #[clap(short, long, value_parser)]
         /// Input FASTA/Q to Filter on Unique / Duplicate Sequences
         input: String,
@@ -28,13 +24,11 @@ enum Commands {
 
         #[clap(short, long, value_parser)]
         /// Filepath to write unique records to
-        null: Option<String>
-
+        null: Option<String>,
     },
 
     /// Creates a mapping of sgRNAs to their parent gene
-    SgrnaTable{
-
+    SgrnaTable {
         #[clap(short, long, value_parser)]
         /// Input FASTA/Q to Generate table
         input: String,
@@ -43,7 +37,7 @@ enum Commands {
         /// Filepath to write table to [default: stdout]
         output: Option<String>,
 
-        #[clap(short='s', long, action)]
+        #[clap(short = 's', long, action)]
         /// Whether to include the sequence in the output table [default: false]
         include_sequence: bool,
 
@@ -55,7 +49,6 @@ enum Commands {
         #[clap(short, long, value_parser)]
         /// Optional choice of output delimiter [default: '\t']
         delim: Option<char>,
-
     },
 
     /// Converts all lowercase nucleotides to uppercase
@@ -67,7 +60,7 @@ enum Commands {
 
         #[clap(short, long, value_parser)]
         /// Filepath to write output to [default: stdout]
-        output: Option<String>
+        output: Option<String>,
     },
 
     /// Creates the Reverse complement for a provided fastx
@@ -78,7 +71,7 @@ enum Commands {
 
         #[clap(short, long, value_parser)]
         /// Filepath to write output to [default: stdout]
-        output: Option<String>
+        output: Option<String>,
     },
 
     /// Filters same length sequences to their variable region. Useful in CRISPRi/a libraries where
@@ -92,35 +85,76 @@ enum Commands {
         /// Filepath to write output to [default: stdout]
         output: Option<String>,
 
-        #[clap(short, long, value_parser, default_value="5000")]
+        #[clap(short, long, value_parser, default_value = "5000")]
         /// Number of samples to calculate positional entropy on
         num_samples: usize,
 
-        #[clap(short, long, value_parser, default_value="1.0")]
+        #[clap(short, long, value_parser, default_value = "1.0")]
         /// Number of samples to calculate positional entropy on
-        zscore_threshold: f64
-    }
-}
+        zscore_threshold: f64,
+    },
 
+    /// Trims adapter sequences that are dynamically placed within the sequence.
+    Trim {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to trim sequences
+        input: String,
+
+        #[clap(short, long, value_parser)]
+        /// Adapater sequence to trim
+        adapter: String,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser, default_value = "false")]
+        /// Trim the adapter off the sequence
+        trim_adapter: bool,
+    },
+}
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Unique { input, output, null } => { 
-            commands::unique::run(&input, output, null)?; 
-        },
-        Commands::SgrnaTable { input, output, include_sequence, delim, reorder } => {
+        Commands::Unique {
+            input,
+            output,
+            null,
+        } => {
+            commands::unique::run(&input, output, null)?;
+        }
+        Commands::SgrnaTable {
+            input,
+            output,
+            include_sequence,
+            delim,
+            reorder,
+        } => {
             commands::sgrna_table::run(&input, output, include_sequence, delim, reorder)?;
-        },
+        }
         Commands::Upper { input, output } => {
             commands::upper::run(&input, output)?;
-        },
+        }
         Commands::Reverse { input, output } => {
             commands::reverse::run(&input, output)?;
-        },
-        Commands::ExtractVariable { input, output, num_samples, zscore_threshold } => {
+        }
+        Commands::ExtractVariable {
+            input,
+            output,
+            num_samples,
+            zscore_threshold,
+        } => {
             commands::extract::run(&input, output, num_samples, zscore_threshold)?;
+        }
+        Commands::Trim {
+            input,
+            output,
+            adapter,
+            trim_adapter,
+        } => {
+            commands::trim::run(&input, &adapter, output, trim_adapter)?;
         }
     };
 
