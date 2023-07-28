d623d46079c0fabd311e7033b25ab6481f272482
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-27 18:05:26 -0700

    added upper and reverse complement inplace functions using mutable closure

--- src/commands/upper.rs
+++ src/commands/upper.rs
@@ -1,16 +1,17 @@
-use super::{match_output_stream, write_output, io::write_output_with_invalid};
+use super::{match_output_stream, io::{write_mut_output, write_mut_output_with_invalid}};
 use anyhow::Result;
 use fxread::{initialize_reader, Record};
 use std::str::from_utf8;
 
 /// Format prints the sequence as uppercase
-fn format_print(record: &Record) -> String {
+fn format_print(record: &mut Record) -> String {
+    record.upper();
     match record.qual() {
         Some(_) => {
             format!(
                 "@{}\n{}\n{}\n{}\n",
                 from_utf8(record.id()).expect("invalid utf8"),
-                from_utf8(&record.seq_upper()).expect("invalid utf8"),
+                from_utf8(record.seq()).expect("invalid utf8"),
                 from_utf8(record.plus().unwrap()).expect("invalid utf8"),
                 from_utf8(record.qual().unwrap()).expect("invalid utf8"),
             )
@@ -19,7 +20,7 @@ fn format_print(record: &Record) -> String {
             format!(
                 ">{}\n{}\n",
                 from_utf8(record.id()).expect("invalid utf8"),
-                from_utf8(&record.seq_upper()).expect("invalid utf8")
+                from_utf8(record.seq()).expect("invalid utf8")
             )
         }
     }
@@ -30,9 +31,9 @@ pub fn run(input: &str, output: Option<String>, num_threads: Option<usize>, comp
     let reader = initialize_reader(input)?;
     let mut writer = match_output_stream(output, num_threads, compression_level)?;
     if allow_invalid {
-        write_output_with_invalid(&mut writer, reader, &format_print);
+        write_mut_output_with_invalid(&mut writer, reader, &format_print);
     } else {
-        write_output(&mut writer, reader, &format_print);
+        write_mut_output(&mut writer, reader, &format_print);
     }
     Ok(())
 }
@@ -40,10 +41,8 @@ pub fn run(input: &str, output: Option<String>, num_threads: Option<usize>, comp
 #[cfg(test)]
 mod test {
     use std::fs::File;
-
-    use crate::commands::io::write_output_with_invalid;
-
-    use super::{format_print, match_output_stream, write_output};
+    use crate::commands::io::write_mut_output_with_invalid;
+    use super::{format_print, match_output_stream, write_mut_output};
     use fxread::{FastaReader, FastqReader, FastxRead, Record};
 
     fn fasta_reader() -> Box<dyn FastxRead<Item = Record>> {
@@ -71,14 +70,14 @@ mod test {
     #[test]
     fn run_fasta() {
         let mut reader = fasta_reader();
-        let upper = reader.next().map(|x| format_print(&x));
+        let upper = reader.next().map(|ref mut x| format_print(x));
         assert_eq!(upper, Some(">ap2s1_asjdajsdas\nACT\n".to_string()));
     }
 
     #[test]
     fn run_fastq() {
         let mut reader = fastq_reader();
-        let upper = reader.next().map(|x| format_print(&x));
+        let upper = reader.next().map(|ref mut x| format_print(x));
         assert_eq!(upper, Some("@ap2s1_asjdajsdas\nACT\n+\n123\n".to_string()));
     }
 
@@ -87,14 +86,14 @@ mod test {
     fn run_invalid_fasta() {
         let reader = invalid_fasta_reader();
         let mut writer = match_output_stream(None, None, None).unwrap();
-        write_output(&mut writer, reader, &format_print)
+        write_mut_output(&mut writer, reader, &format_print)
     }
 
     #[test]
     fn run_invalid_fasta_allow_invalid() {
         let reader = invalid_fasta_reader();
         let mut writer = File::create("/dev/null").unwrap();
-        write_output_with_invalid(&mut writer, reader, &format_print)
+        write_mut_output_with_invalid(&mut writer, reader, &format_print)
     }
 
     #[test]
@@ -102,13 +101,13 @@ mod test {
     fn run_invalid_fastq() {
         let reader = invalid_fastq_reader();
         let mut writer = match_output_stream(None, None, None).unwrap();
-        write_output(&mut writer, reader, &format_print)
+        write_mut_output(&mut writer, reader, &format_print)
     }
 
     #[test]
     fn run_invalid_fastq_allow_invalid() {
         let reader = invalid_fastq_reader();
         let mut writer = File::create("/dev/null").unwrap();
-        write_output_with_invalid(&mut writer, reader, &format_print)
+        write_mut_output_with_invalid(&mut writer, reader, &format_print)
     }
 }
