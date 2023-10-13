43389c9f9b16fe5906fd47489f9000f8f02809c6
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-10-12 21:05:38 -0700

    style: reorder cli to be alphabetical

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,92 @@ enum Commands {
         input: Option<String>,
     },
 
-    /// Filters the Fastx file for Unique Sequences
-    Unique {
+    /// Filters same length sequences to their variable region. Useful in CRISPRi/a libraries where
+    /// the variable region is prefixed and suffixed by some constant region
+    ExtractVariable {
         #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to Filter on Unique / Duplicate Sequences
+        /// Input FASTA/Q to to extract variable region
+        input: String,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser, default_value = "5000")]
+        /// Number of samples to calculate positional entropy on
+        num_samples: usize,
+
+        #[clap(short, long, value_parser, default_value = "1.0")]
+        /// Number of samples to calculate positional entropy on
+        zscore_threshold: f64,
+    },
+
+    /// Filters a fastx file by searching for whether they follow a regex pattern on the sequence
+    Filter {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to fix
         input: Option<String>,
 
         #[clap(short, long, value_parser)]
-        /// Filepath to write unique records to [default: stdout]
+        /// Filepath to write output to [default: stdout]
         output: Option<String>,
 
         #[clap(short, long, value_parser)]
-        /// Filepath to write unique records to
-        null: Option<String>,
+        /// Regex pattern to search for
+        pattern: String,
+
+        #[clap(short = 'v', long, value_parser, default_value = "false")]
+        /// Whether to invert the filter
+        invert: bool,
+
+        #[clap(short = 'H', long, value_parser, default_value = "false")]
+        /// Whether to search for the pattern in the header
+        header: bool,
+    },
+
+    /// Fix a fastx file by replacing invalid characters with N
+    Fix {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to fix
+        input: Option<String>,
 
         #[clap(short, long, value_parser)]
-        /// Allow invalid nucleotides in output
-        allow_invalid: bool,
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+    },
+
+    /// Creates the Reverse complement for a provided fastx
+    Reverse {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to Convert to Upper
+        input: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+    },
+
+    /// Samples a fastx file by a frequency
+    Sample {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to sample
+        input: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser, default_value = "0.5")]
+        /// Frequency to sample by
+        frequency: f64,
+
+        #[clap(short, long, value_parser)]
+        /// Seed to use for sampling
+        seed: Option<u64>,
+
+        #[clap(short, long, value_parser, default_value = "false")]
+        /// Don't write number of records sampled to stderr
+        quiet: bool,
     },
 
     /// Creates a mapping of sgRNAs to their parent gene
@@ -68,95 +137,6 @@ enum Commands {
         #[clap(short, long, value_parser)]
         /// Optional choice of output delimiter [default: '\t']
         delim: Option<char>,
-    },
-
-    /// Converts all lowercase nucleotides to uppercase
-    /// and validates for unexpected nucleotides
-    Upper {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to Convert to Upper
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Allow invalid nucleotides in output
-        allow_invalid: bool,
-    },
-
-    /// Creates the Reverse complement for a provided fastx
-    Reverse {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to Convert to Upper
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-    },
-
-    /// Filters same length sequences to their variable region. Useful in CRISPRi/a libraries where
-    /// the variable region is prefixed and suffixed by some constant region
-    ExtractVariable {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to to extract variable region
-        input: String,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-
-        #[clap(short, long, value_parser, default_value = "5000")]
-        /// Number of samples to calculate positional entropy on
-        num_samples: usize,
-
-        #[clap(short, long, value_parser, default_value = "1.0")]
-        /// Number of samples to calculate positional entropy on
-        zscore_threshold: f64,
-    },
-
-    /// Trims adapter sequences that are dynamically placed within the sequence.
-    Trim {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to trim sequences
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Adapater sequence to trim
-        adapter: String,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-
-        #[clap(short, long, value_parser, default_value = "false")]
-        /// Trim the adapter off the sequence
-        trim_adapter: bool,
-    },
-
-    /// Samples a fastx file by a frequency
-    Sample {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to sample
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-
-        #[clap(short, long, value_parser, default_value = "0.5")]
-        /// Frequency to sample by
-        frequency: f64,
-
-        #[clap(short, long, value_parser)]
-        /// Seed to use for sampling
-        seed: Option<u64>,
-
-        #[clap(short, long, value_parser, default_value = "false")]
-        /// Don't write number of records sampled to stderr
-        quiet: bool,
     },
 
     /// Sorts a fastx file by sequence
@@ -184,40 +164,6 @@ enum Commands {
         sort_by_r1: bool,
     },
 
-    /// Fix a fastx file by replacing invalid characters with N
-    Fix {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to fix
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-    },
-
-    /// Filters a fastx file by searching for whether they follow a regex pattern on the sequence
-    Filter {
-        #[clap(short, long, value_parser)]
-        /// Input FASTA/Q to fix
-        input: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Filepath to write output to [default: stdout]
-        output: Option<String>,
-
-        #[clap(short, long, value_parser)]
-        /// Regex pattern to search for
-        pattern: String,
-
-        #[clap(short = 'v', long, value_parser, default_value = "false")]
-        /// Whether to invert the filter
-        invert: bool,
-
-        #[clap(short = 'H', long, value_parser, default_value = "false")]
-        /// Whether to search for the pattern in the header
-        header: bool,
-    },
-
     /// Extracts the transcript to gene mapping from an ensembl cdna fasta file
     T2g {
         #[clap(short, long, value_parser)]
@@ -238,6 +184,60 @@ enum Commands {
         /// Defaults to clipping the dot version
         dot_version: bool,
     },
+
+    /// Trims adapter sequences that are dynamically placed within the sequence.
+    Trim {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to trim sequences
+        input: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Adapater sequence to trim
+        adapter: String,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser, default_value = "false")]
+        /// Trim the adapter off the sequence
+        trim_adapter: bool,
+    },
+
+    /// Filters the Fastx file for Unique Sequences
+    Unique {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to Filter on Unique / Duplicate Sequences
+        input: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write unique records to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write unique records to
+        null: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Allow invalid nucleotides in output
+        allow_invalid: bool,
+    },
+
+    /// Converts all lowercase nucleotides to uppercase
+    /// and validates for unexpected nucleotides
+    Upper {
+        #[clap(short, long, value_parser)]
+        /// Input FASTA/Q to Convert to Upper
+        input: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Filepath to write output to [default: stdout]
+        output: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        /// Allow invalid nucleotides in output
+        allow_invalid: bool,
+    },
 }
 
 fn main() -> Result<()> {
@@ -246,68 +246,6 @@ fn main() -> Result<()> {
     match cli.command {
         Commands::Count { input } => {
             commands::count::run(input)?;
-        }
-        Commands::Sample {
-            input,
-            output,
-            frequency,
-            seed,
-            quiet,
-        } => {
-            commands::sample::run(
-                input,
-                output,
-                frequency,
-                seed,
-                quiet,
-                cli.compression_threads,
-                cli.compression_level,
-            )?;
-        }
-        Commands::Unique {
-            input,
-            output,
-            null,
-            allow_invalid,
-        } => {
-            commands::unique::run(
-                input,
-                output,
-                null,
-                cli.compression_threads,
-                cli.compression_level,
-                allow_invalid,
-            )?;
-        }
-        Commands::SgrnaTable {
-            input,
-            output,
-            include_sequence,
-            delim,
-            reorder,
-        } => {
-            commands::sgrna_table::run(input, output, include_sequence, delim, reorder)?;
-        }
-        Commands::Upper {
-            input,
-            output,
-            allow_invalid,
-        } => {
-            commands::upper::run(
-                input,
-                output,
-                cli.compression_threads,
-                cli.compression_level,
-                allow_invalid,
-            )?;
-        }
-        Commands::Reverse { input, output } => {
-            commands::reverse::run(
-                input,
-                output,
-                cli.compression_threads,
-                cli.compression_level,
-            )?;
         }
         Commands::ExtractVariable {
             input,
@@ -320,46 +258,6 @@ fn main() -> Result<()> {
                 output,
                 num_samples,
                 zscore_threshold,
-                cli.compression_threads,
-                cli.compression_level,
-            )?;
-        }
-        Commands::Trim {
-            input,
-            output,
-            adapter,
-            trim_adapter,
-        } => {
-            commands::trim::run(
-                input,
-                &adapter,
-                output,
-                trim_adapter,
-                cli.compression_threads,
-                cli.compression_level,
-            )?;
-        }
-        Commands::Sort {
-            r1,
-            r2,
-            prefix,
-            gzip,
-            sort_by_r1,
-        } => {
-            commands::sort::run(
-                r1,
-                r2,
-                prefix,
-                gzip,
-                sort_by_r1,
-                cli.compression_threads,
-                cli.compression_level,
-            )?;
-        }
-        Commands::Fix { input, output } => {
-            commands::fix::run(
-                input,
-                output,
                 cli.compression_threads,
                 cli.compression_level,
             )?;
@@ -381,6 +279,65 @@ fn main() -> Result<()> {
                 cli.compression_level,
             )?;
         }
+        Commands::Fix { input, output } => {
+            commands::fix::run(
+                input,
+                output,
+                cli.compression_threads,
+                cli.compression_level,
+            )?;
+        }
+        Commands::Reverse { input, output } => {
+            commands::reverse::run(
+                input,
+                output,
+                cli.compression_threads,
+                cli.compression_level,
+            )?;
+        }
+        Commands::Sample {
+            input,
+            output,
+            frequency,
+            seed,
+            quiet,
+        } => {
+            commands::sample::run(
+                input,
+                output,
+                frequency,
+                seed,
+                quiet,
+                cli.compression_threads,
+                cli.compression_level,
+            )?;
+        }
+        Commands::SgrnaTable {
+            input,
+            output,
+            include_sequence,
+            delim,
+            reorder,
+        } => {
+            commands::sgrna_table::run(input, output, include_sequence, delim, reorder)?;
+        }
+        Commands::Sort {
+            r1,
+            r2,
+            prefix,
+            gzip,
+            sort_by_r1,
+        } => {
+            commands::sort::run(
+                r1,
+                r2,
+                prefix,
+                gzip,
+                sort_by_r1,
+                cli.compression_threads,
+                cli.compression_level,
+            )?;
+        }
         Commands::T2g {
             input,
             output,
@@ -394,6 +351,49 @@ fn main() -> Result<()> {
                 dot_version,
                 cli.compression_threads,
                 cli.compression_level,
+            )?;
+        }
+        Commands::Trim {
+            input,
+            output,
+            adapter,
+            trim_adapter,
+        } => {
+            commands::trim::run(
+                input,
+                &adapter,
+                output,
+                trim_adapter,
+                cli.compression_threads,
+                cli.compression_level,
+            )?;
+        }
+        Commands::Unique {
+            input,
+            output,
+            null,
+            allow_invalid,
+        } => {
+            commands::unique::run(
+                input,
+                output,
+                null,
+                cli.compression_threads,
+                cli.compression_level,
+                allow_invalid,
+            )?;
+        }
+        Commands::Upper {
+            input,
+            output,
+            allow_invalid,
+        } => {
+            commands::upper::run(
+                input,
+                output,
+                cli.compression_threads,
+                cli.compression_level,
+                allow_invalid,
             )?;
         }
     };
