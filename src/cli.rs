a11390f8d0472a4d9e8060510d19423d330b528c
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-10-08 10:49:06 -0700

    feat: added colored clap

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,22 @@
-use clap::{Parser, Subcommand};
+use clap::{
+    builder::{
+        styling::{AnsiColor, Effects},
+        Styles,
+    },
+    Parser, Subcommand,
+};
 
 use crate::commands::csv::Delimiter;
 
+// Configures Clap v3-style help menu colors
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Green.on_default().effects(Effects::BOLD))
+    .usage(AnsiColor::Green.on_default().effects(Effects::BOLD))
+    .literal(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+    .placeholder(AnsiColor::Cyan.on_default());
+
 #[derive(Parser)]
+#[command(styles = STYLES)]
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
     #[clap(subcommand)]
